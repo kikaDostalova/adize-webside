08c75620504f0a9ea6253961088c903a1a5d4888
kika.dostalova <dostalovak265@gmail.com>
2026-04-11 22:04:55 +0200

    stavby

--- gallery.js
+++ gallery.js
@@ -7,15 +7,9 @@ for (let i = 1; i <= 39; i++) {
 
 const gallery = document.getElementById("gallery");
 
-images.forEach(img => {
-    const element = document.createElement("img");
-    element.src = `images/${img}`;
-    element.alt = "Ukážka práce";
-    gallery.appendChild(element);
-});
-
 document.querySelectorAll(".more-btn").forEach(button => {
     button.addEventListener("click", function () {
+
         const id = this.getAttribute("data-target");
         const box = document.getElementById(id);
 
@@ -28,5 +22,55 @@ document.querySelectorAll(".more-btn").forEach(button => {
         }
     });
 });
+
+
+images.forEach(img => {
+    const element = document.createElement("img");
+    element.src = `images/${img}`;
+    element.alt = "Ukážka práce";
+    gallery.appendChild(element);
+});
+
+// funkcia na zobrazenie toastu
+function showToast(message, type = "success") {
+    const toast = document.createElement("div");
+    toast.classList.add("toast-message");
+
+    // farebné varianty
+    if (type === "error") {
+        toast.style.background = "#E00000";
+    } else {
+        toast.style.background = "#008a2e";
+    }
+
+    toast.textContent = message;
+
+    document.getElementById("toast").appendChild(toast);
+
+    // odstránenie po 4s
+    setTimeout(() => {
+        toast.remove();
+    }, 4000);
+}
+
+document.getElementById("contactForm").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+    });
+
+    if (response.ok) {
+        showToast("Vaša správa bola úspešne odoslaná. Ďakujeme!", "success");
+        form.reset();
+    } else {
+        showToast("Nastala chyba pri odosielaní formulára. Skúste znova.", "error");
+    }
+});
+
 
 
